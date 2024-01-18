710001317177cdab7e34eed88f32032a1b5d217a
Kivuila Nahel <nahel.kivuila@gmail.com>
2024-01-18 17:28:13 +0100

    B: refactoring

--- src/currency_convertor.py
+++ src/currency_convertor.py
@@ -3,12 +3,12 @@ class CurrencyConvertor:
         convert = 0
         try:
             amount = float(amount)
-            
+        
             if amount < 0:
                 raise SyntaxError()
+            
             convert = round(amount*0.94, 1) 
         except SyntaxError:
-            print(amount)
             print('Entrez une valeur positive')
             convert = 0
         except TypeError:
