f55f99cc039e4ad9ff6e8e827a12af6b3cf6cf87
Kivuila Nahel <nahel.kivuila@gmail.com>
2024-01-18 16:19:29 +0100

    G: faire que le test passer avec un handler for the exeptions

--- src/currency_convertor.py
+++ src/currency_convertor.py
@@ -2,8 +2,14 @@ class CurrencyConvertor:
     def Convertor(amount):
         convert = 0
         try:
-            convert = round(float(amount)*0.94, 1) 
+            amount = float(amount)
+            if (amount<0):
+                raise ValueError()
+            convert = round(amount*0.94, 1) 
+        except ValueError:
+            print('Entrez une valeur positive')
+            convert = 0
         except:
             convert = 0
-        
+
         return convert
