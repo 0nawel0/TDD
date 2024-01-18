b195dd7a3022eac77bc1ba02d37c25ef4c32ca5a
Kivuila Nahel <nahel.kivuila@gmail.com>
2024-01-18 15:43:59 +0100

    R: add test with a null value

--- test/test_currency_convertor.py
+++ test/test_currency_convertor.py
@@ -4,11 +4,20 @@ from src.currency_convertor import CurrencyConvertor
 @pytest.mark.parametrize("eur, chf", [
     (10,9.4),
     (20, 18.8),
-    (30, 28.2) # test case
+    (30, 28.2), # test case
 ])
 def test_Convert_10_Eur_To_Chf(eur, chf):
 
     # Appel la fonction qui converti les eur en chf
+    convert = CurrencyConvertor.Convertor(eur)
+
+    assert convert == chf
+
+
+def test_Convert_Null_Value():
+    eur = None
+    chf = 0
+
     convert = CurrencyConvertor.Convertor(eur)
 
     assert convert == chf
