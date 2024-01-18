1159e82aa26852baf460f377fd9cd589f327cd1f
Kivuila Nahel <nahel.kivuila@gmail.com>
2024-01-18 14:46:37 +0100

    2eme iteration 1ere phase / Deuxième cas de test avec d'autre valeurs

--- test/test_currency_convertor.py
+++ test/test_currency_convertor.py
@@ -2,8 +2,8 @@ import pytest
 from src.currency_convertor import CurrencyConvertor
 
 def test_Convert_Eur_To_Chf():
-    eur = 10
-    eur_To_Chf = 9.4
+    eur = 20
+    eur_To_Chf = 18.8
 
     # Appel la fonction qui converti les eur en chf
     convert = CurrencyConvertor.Convertor(eur)
